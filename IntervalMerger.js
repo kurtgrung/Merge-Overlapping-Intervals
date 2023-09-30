8573362dceb9575a6b00743172760900a9731cf1
“Kurt Grüng” <114594062+kurtgrung@users.noreply.github.com>
2023-09-30 15:58:09 +0100

    Alternative version
    Code does exactly the same thing :)

--- IntervalMerger.js
+++ IntervalMerger.js
@@ -2,29 +2,30 @@ class IntervalMerger {
     merge(intervals) {
         if (intervals.length <= 1) return intervals;
 
+        const mergedIntervals = [];
         intervals.sort((a, b) => a[0] - b[0]);
 
-        const mergedIntervals = [intervals[0]];
+        intervals.forEach(function (currentInterval, i) {
 
-        for (let i = 1; i < intervals.length; i++) {
-            const currentInterval = intervals[i];
-            const previousInterval = mergedIntervals[mergedIntervals.length - 1];
+            const previousInterval = //Conditional (ternary) operator
+                i > 0 ? mergedIntervals[mergedIntervals.length - 1] : 0;
 
             if (currentInterval[0] <= previousInterval[1]) {
                 previousInterval[1] = Math.max(previousInterval[1], currentInterval[1]);
             } else {
                 mergedIntervals.push(currentInterval);
             }
-        }
+        });
 
         return mergedIntervals;
     }
 }
 
-const intervals = [[1, 3],[2, 6],[8, 10],[15, 18]];
+const intervals = [[1, 3], [2, 6], [8, 10], [15, 18]];
 const intervalMerger = new IntervalMerger();
 const updatedIntervals = intervalMerger.merge(intervals);
 
+// output
 console.log(updatedIntervals);
 console.log(`Updated Intervals: ${JSON.stringify(updatedIntervals)}`);
 document.getElementById("output").innerHTML = `Updated Intervals: ${JSON.stringify(updatedIntervals)}`;
